36b48072c8ddffec7ecf28e274bdef040fde0c93
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-10-10 15:18:35 -0400

    Add personal budget content

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bottom">
+      <div className="center">
+        All rights reserved &copy;
+        <span>Fabio Nolasco</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
